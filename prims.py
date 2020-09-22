c475a2e37e8e61f3975943994739030774df0364
Yusuf Mukadam <yusuf.mukadam@gmail.com>
2020-09-22 16:40:32 +0200

    made a couple of fixes so there were no more errors, and added some tests to check how some parts are working

--- prims.py
+++ prims.py
@@ -1,7 +1,8 @@
 import heapq
 import sys
 
-MAX_INT = sys.MAX_INT
+MAX_INT = 9223372036854775807
+
 
 #  1 2 3 4 
 #1 0 
@@ -12,9 +13,11 @@ MAX_INT = sys.MAX_INT
 class node(object):
 
 	def __init__(self, node):
-		node = node
-		key = MAX_INT
-		parent = -1
+		self.node = node
+		self.key = MAX_INT
+		self.parent = None
+
+	def get_key(self): return self.key
 
 
 class matrix(object):
@@ -22,27 +25,42 @@ class matrix(object):
 	def __init__(self, matrix:list ):
 		self.adj = matrix
 
-	def get_weight(u,v):
-		return matrix[u][v]
+	def get_weight(self, u, v):
+		return self.adj[u][v]
 
-	def get_adj(u):
+	def get_adj(self, u):
 		nodes = []
-		for i in self.adj[u]:
-			if i > 0:
-				nodes.append(i)
+		for index, weight in enumerate(self.adj[u]):
+			if weight > 0:
+				nodes.append(index)
 		return nodes
 
+
 def MST_PRIM(G:matrix , r:int):
+	mst = []
 	Q = []
-	for u in G.adj:
-		Q.append(node(u))
+	for i in range(len(G.adj)):
+		Q.append(node(i))
 	Q[r].key = 0
 	while Q:
-		u = min(Q, key=key)
-		Q.remove(u)
-		for v in G.get_adj():
+		u = min(Q, key=lambda k: k.key)
+		for v in G.get_adj(u.node):
 			for i in Q:
-				if v == i.node and i.key > G.get_weight(u.node,v):
+				if v == i.node and i.key > G.get_weight(u.node, v):
 					i.parent = u.node
-					i.key = G.get_weight(u.node,v)
+					i.key = G.get_weight(u.node, v)
+		mst.append(u)
+		Q.remove(u)
+	return mst
 
+
+root = 1
+
+				# 0  1  2  3
+graph = matrix(	[[0,-1,2,3],
+			   	[ -1,0,2,1],
+				[  2,2,0,5],
+				[  3,1,5,0]])
+mst = MST_PRIM(graph, root)
+for node in mst:
+	print(node.node, node.parent)
