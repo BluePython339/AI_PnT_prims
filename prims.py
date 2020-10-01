8f2509d963cfd9d3cf6698d7a45609cd927f8bd6
Bluepython339 <bart.hofman20@gmail.com>
2020-10-01 23:33:58 +0200

    new implementation that kind of works

--- prims.py
+++ prims.py
@@ -19,6 +19,9 @@ class node(object):
 		self.key = key
 		self.parent = None
 
+	def __str__(self):
+		return("name: {} key: {}".format(self.node, self.key))
+
 	def get_key(self): 
 		return self.key
 
@@ -28,7 +31,7 @@ class node(object):
 
 class matrix(object):
 
-	def __init__(self, matrix:list ):
+	def __init__(self, matrix:list):
 		self.adj = matrix
 
 	def get_weight(self, u, v):
@@ -41,6 +44,12 @@ class matrix(object):
 				nodes.append(index)
 		return nodes
 
+	def __str__(self):
+		full = ""
+		for i in range(len(self.adj)):
+			full += str(self.adj[i])+"\n"
+		return full
+
 
 def MST_PRIM(G:matrix , r:int):
 	mst = []
@@ -52,6 +61,11 @@ def MST_PRIM(G:matrix , r:int):
 			h.heappush(Q, node(i, MAX_INT))
 	while Q:
 		u = h.heappop(Q)
+		test = "current pick: {}"
+		print(test.format(u))
+		for i in Q:
+			print(i)
+		print("___________________________________")
 		for v in G.get_adj(u.node):
 			for i in Q:
 				if v == i.node and i.key > G.get_weight(u.node, v):
@@ -67,42 +81,52 @@ def random_graph(vertices:int, edges:int, min_w:int, max_w:int)-> matrix:
 		return [[0,0]]
 
 	gr = np.array(np.zeros(vertices**2)).reshape(vertices,vertices)
+	gr.fill(-1)
 	placed = []
+	for i in range(vertices):
+		gr[i,i] = 0
 	for a in range(edges):
-		i = random.choice([i for i in range(vertices)])
-		j = random.choice([i for i in range(vertices)])
+		i = np.random.randint(0, vertices)
+		j = np.random.randint(0, vertices)
 		if (i != j) or ((i,j) in placed):
-			gr[i j] = np.random.randint(min_w, max_w+1)
+			weight = np.random.randint(min_w, max_w+1)
+			gr[i,j] = weight
+			gr[j,i] = weight
+			placed.append((i,j))
+			placed.append((j,i))
 		edges -= 1
 
 	for  i in range(vertices):
-		if sum(gr[i]) <= len(j-1)*-1:
+		if sum(gr[i]) <= vertices-1*-1:
 			edges -= 1
 			randpos = i
 			while randpos == i:
-				randpos = np.random.randint(0, vertices+1)
-			gr[i randpos] = np.random.randint(min_w, max_w+1)
+				randpos = np.random.randint(0, vertices)
+			gr[i,randpos] = np.random.randint(min_w, max_w+1)
 
 	while edges < 0:
-		i = np.random.randint(0,vertices+1)
+		i = np.random.randint(0,vertices)
 		if (sum(gr[i]) >= max_w) and edges < 0: #pretty much means that it has more than 1 connection
 			edges += 1
 			while randpos == i:
-				randpos = np.random.randint(0, vertices+1)
-			gr[i randpos] = -1
+				randpos = np.random.randint(0, vertices)
+			gr[i,randpos] = -1
 
 	return matrix(gr)
 
-root = 1
+
+if __name__ == "__main__":
+	root = 1
 
 				# 0  1  2  3
-graph = matrix(	[[0,-1,2,3],
-			   	[ -1,0,2,1],
-				[  2,2,0,5],
-				[  3,1,5,0]])
+	graph = matrix(	[[0,-1,2,3],
+				   	[ -1,0,2,1],
+					[  2,2,0,5],
+					[  3,1,5,0]])
 
-graph = random_graph(4, 3, 1, 10)
+	graph = random_graph(5, 10, 1, 15)
+	print(graph)
 
-mst = MST_PRIM(graph, root)
-for node in mst:
-	print(node.node, node.parent)
+	mst = MST_PRIM(graph, root)
+	for node in mst:
+		print(node.node, node.parent)
