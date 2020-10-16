27c8fa0b5665c0d9115aabd4cc20d0c21b77bc13
Bluepython339 <bart.hofman20@gmail.com>
2020-10-16 17:11:02 +0200

    only 1 more test needed

--- PULP.py
+++ PULP.py
@@ -1,14 +1,15 @@
 from pulp import*
 
+#function takes a n*n matrix of values supplied by pulp 
+#prints a chessboard like structure that shows where the queens are placed
 def chess_me_a_board(choices, n):
-	#print((n-int((n/4)))*"___")
 	for i in range(n):
 		pstring = "|"
 		for x in range(n):
 			pstring += ("Q" if int(value(choices[i][x])) == 1 else " ") +"|"
 		print(pstring)
-		#print((n-int((n/4)))*"___")
 
+#function that calculates the NQueens problem, takes in an int and returns the solution
 def queens_me_a_place(n):
 	ROWS = COLS = range(n)
 
@@ -16,14 +17,15 @@ def queens_me_a_place(n):
 
 
 	choices = LpVariable.dicts("Fill", (ROWS, COLS),0,1, LpInteger)
-	prob += 0 ,"tests"
+	#prob += 0 ,"tests"
 	print(choices)
 
+#constraints for columns and rows
 	for r in ROWS:
 	    prob += lpSum([choices[r][c] for c in COLS]) == 1, 'rows({})'.format(r)
 	    prob += lpSum([choices[c][r] for c in COLS]) == 1, 'cols({})'.format(r)
 
-
+#constraints for the diagonals
 	for k in range(2-n, n-1):
 		prob += lpSum([(choices[r][c] if r-c == k else None) for c in COLS]for r in ROWS) <= 1, 'dig1({})'.format(k)
 
@@ -36,16 +38,16 @@ def queens_me_a_place(n):
 
 	solved = prob.solve()
 	if solved:
-		chess_me_a_board(choices,n)
+		chess_me_a_board(choices,n) #draw the chessboard
 
 if __name__ == "__main__":
 	n = input("how many squares ya want: ") 
-	if not n.isdigit():
+	if not n.isdigit(): #check for ints
 		print("we only take ints here")
 		exit()
 	else:
-		if int(n) > 3 :
-			queens_me_a_place(int(n))
+		if int(n) > 3 : #n = 1 is a trivial case so ignored
+			queens_me_a_place(int(n))  #calculate a board setting
 		else:
 			print("thats a non starter")
 			exit()
